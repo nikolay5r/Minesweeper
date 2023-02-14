837ec26ff9e1af3d145e54888c83d5c290a9115f
nikolay5r <nradilov30@gmail.com>
2023-02-14 14:04:15 +0200

    Add difficulty choice function

--- Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+const int DIFFICULTY_SIZES[3] = { 12, 18, 24 };
+
+int difficultyChosen()
+{
+	std::cout << "Choose difficulty of the game: \n"
+		<< "\t0: easy\n"
+		<< "\t1: medium\n"
+		<< "\t2: hard\n";
+	int chosenDifficulty = 0;
+	std::cin >> chosenDifficulty;
+
+	return chosenDifficulty;
+}
+
 int main()
 {
-	std::cout << "Hello World!";
+	
+	
+
 	return 0;
 }
